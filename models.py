ac8689ffc2d9fab4767174569e2a8cfea2e5b822
bevennyamande <bevennyamande@gmail.com>
2019-04-02 00:45:32 +0200

    adding user auth

--- models.py
+++ models.py
@@ -1,11 +1,24 @@
 from tortoise.models import Model
 from tortoise import fields
 
+class User(Model):
+	email = fields.charField(max_length=80)
+	password = fields.charField(max_length=80)
+
+	def __init__(self, email: str, password: str):
+		self.email = email
+		self.password = password
+
+	def __str__(self):
+		return f'''User is {self.email} {self.password}'''
+
+
 
 class Post(Model):
     title = fields.CharField(max_length=80)
     content = fields.TextField()
     category = fields.ForeignKeyField("models.Category", related_name="posts")
+    userid = fields.ForeignKeyField("models.User", related_name="posts")
 
     def __str__(self):
         return self.title
